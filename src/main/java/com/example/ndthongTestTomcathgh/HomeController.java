47d5ee6a62ebbd18ecbc778d039470479c5ba13f
DinhThong <74182542+THONG249@users.noreply.github.com>
2022-12-09 14:09:13 +0700

    Update HomeController.java
    this is new commit

--- src/main/java/com/example/ndthongTestTomcathgh/HomeController.java
+++ src/main/java/com/example/ndthongTestTomcathgh/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 @GetMapping
 String Index() {
-	return "Index.html";
+	return "Ngo dinh thong tesst";
 }
 }
